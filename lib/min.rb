2125fe16407a27ec58bdbc71449d62509d0ecac6
adeeba reaz <adeebarivera@outlook.com>
2020-04-25 17:57:15 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -25,10 +25,3 @@ groceries = {
   grains: ["rice", "pasta"]
 }
 
-
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
-}
