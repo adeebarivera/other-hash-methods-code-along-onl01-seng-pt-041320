d33aa357f5b64a10318ef756004f930ea592d83c
adeeba reaz <adeebarivera@outlook.com>
2020-04-25 17:56:20 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -8,12 +8,12 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_min(groceries)
-  letters = {a: 1, b: 2}
+# def get_the_min(groceries)
+#   letters = {a: 1, b: 2}
  
-letters.min
-#  => [:a, 1]
+# letters.min
+# #  => [:a, 1]
  
-alphabetical_order = {c: 100, d: 1}
-alphabetical_order.min
-end
+# alphabetical_order = {c: 100, d: 1}
+# alphabetical_order.min
+# end
