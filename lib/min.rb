ecc8a0c099f5596b55be7df71abf581c12121c07
adeeba reaz <adeebarivera@outlook.com>
2020-04-25 17:56:30 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -17,3 +17,12 @@ require 'pry'
 # alphabetical_order = {c: 100, d: 1}
 # alphabetical_order.min
 # end
+
+
+
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
